940a4a8de220601fc4096e1a279b1a8d9a5fe04c
Anastas Avdzhiev <anastas.avdzhiev@gmail.com>
2016-11-03 17:17:53 +0200

    fixed some css and js

--- docs/main.js
+++ docs/main.js
@@ -1,12 +1,42 @@
-console.log("Танита пръцка!");
-function changeClass(btn) {
-    var button = document.getElementById(btn);
+window.onload = function() {
+  stateRetrieve ('btn');
+  stateRetrieve ('btn2');
+}
+
+
+function changeClass(bttn) {
+    var button = document.getElementById(bttn);
     if (button.className == "btn-empty") {
       button.className = "btn-occupied";
-      console.log("Occupied class works!)");
-      button.innerHTML = "Occupied"
-    } else {
+      console.log("Class changed to 'btn-occupied'!");
+
+      button.innerHTML = "Occupied";
+      console.log("Text changed to 'Occupied'!")
+
+    } else if (button.className == "btn-occupied") {
       button.className = "btn-empty";
-      button.innerHTML = "Free"
+      button.innerHTML = "Free";
     }
+
+    localStorage.setItem(bttn, button.innerHTML);
+    console.log(bttn + " is saved in local storage.")
   };
+
+function stateRetrieve (bttn) {
+  if (typeof(Storage) !== "undefined") {
+      // Retrieve
+      var button = document.getElementById(bttn)
+      button.innerHTML = localStorage.getItem(bttn);
+
+      if (button.innerHTML == 'Occupied') {
+        button.className = "btn-occupied";
+      } else if (button.innerHTML == 'Free') {
+        button.className = "btn-empty";
+      }
+
+      console.log("State is retrieved.");
+      
+    } else {
+      document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}
